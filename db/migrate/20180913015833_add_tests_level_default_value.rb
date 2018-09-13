b359e4f2a9e9bd5a259c0d2277384e747f369c51
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2018-09-13 05:01:40 +0300

    Fix from default value in migration add_tests_level_default_value

--- db/migrate/20180913015833_add_tests_level_default_value.rb
+++ db/migrate/20180913015833_add_tests_level_default_value.rb
@@ -1,5 +1,5 @@
 class AddTestsLevelDefaultValue < ActiveRecord::Migration[5.2]
   def change
-    change_column_default :tests, :level, from: NULL, to: 0
+    change_column_default :tests, :level, from: nil, to: 0
   end
 end
