2ae8787bb878fc4f0fc14471d6a84727e89c8d5a
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2018-09-13 04:30:54 +0300

    Migrate model model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_012949) do
+ActiveRecord::Schema.define(version: 2018_09_13_013037) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text"
     t.boolean "correct"
     t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
