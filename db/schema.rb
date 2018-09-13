ced719af07aee26359929806fa55bcd5db743320
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2018-09-13 04:30:07 +0300

    Migrate model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_012921) do
+ActiveRecord::Schema.define(version: 2018_09_13_012949) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2018_09_13_012921) do
     t.string "title"
     t.integer "level"
     t.integer "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
