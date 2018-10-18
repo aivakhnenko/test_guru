4d43347d2a54f9260a000c741df507250dc5ffa2
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2018-10-18 14:36:43 +0300

    Lesson 13

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,9 +16,20 @@ module ApplicationHelper
     flash_tag(flash_type) if flash[flash_type]
   end
 
+  def change_language
+    locales_names = { ru: 'Русский', en: 'English' }
+    target_locale = I18n.locale == :ru ? :en : :ru
+    target_locale_name = locales_names[target_locale]
+    target_locale = nil if I18n.default_locale == target_locale
+    link_to target_locale_name, url_for(lang: target_locale)
+  end
+
   private
 
   def flash_tag(flash_type)
-    content_tag :p, flash[flash_type], class: "flash #{flash_type}"
+    flash_type_to_alert_type = { alert: :danger, notice: :info }
+    alert_type = flash_type_to_alert_type[flash_type.to_sym] || :other
+    alert_class = "alert alert-#{alert_type}"
+    content_tag :div, flash[flash_type], class: alert_class, role: 'alert'
   end
 end
