e51ffa91ab369ce2159fe2f910c0388b1993929e
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2018-10-23 17:43:00 +0300

    Lesson 15 - fix after code review

--- app/assets/javascripts/utilities/password_confirmation.js
+++ app/assets/javascripts/utilities/password_confirmation.js
@@ -1,24 +1,24 @@
 document.addEventListener('turbolinks:load', function() {
-  var control = document.querySelector(".check-password-confirmation")
+  var control = document.querySelector(".check-password-confirmation");
 
-  if (control) { control.addEventListener('input', checkPasswordConfirmation) }
+  if (control) { control.addEventListener('input', checkPasswordConfirmation); }
 })
 
 function checkPasswordConfirmation() {
-  var icon_ok = this.parentNode.querySelector(".octicon-check")
-  var icon_no = this.parentNode.querySelector(".octicon-x")
-  var pass2 = this.value
+  var iconOk = this.parentNode.querySelector(".octicon-check");
+  var iconNo = this.parentNode.querySelector(".octicon-x");
+  var pass2 = this.value;
   if (pass2) {
-    pass1 = document.querySelector("input[id='user_password']").value
+    pass1 = document.querySelector("input[id='user_password']").value;
     if (pass1 == pass2) {
-      icon_ok.classList.remove('hide')
-      icon_no.classList.add('hide')
+      iconOk.classList.remove('hide');
+      iconNo.classList.add('hide');
     } else {
-      icon_ok.classList.add('hide')
-      icon_no.classList.remove('hide')
+      iconOk.classList.add('hide');
+      iconNo.classList.remove('hide');
     }
   } else {
-    icon_ok.classList.add('hide')
-    icon_no.classList.add('hide')
+    iconOk.classList.add('hide');
+    iconNo.classList.add('hide');
   }
 }
--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -1,58 +1,58 @@
 document.addEventListener('turbolinks:load', function() {
-  var control = document.querySelector('.sort-by-title')
+  var control = document.querySelector('.sort-by-title');
 
-  if (control) { control.addEventListener('click', sortRowsByTitle) }
-})
+  if (control) { control.addEventListener('click', sortRowsByTitle); }
+});
 
 function sortRowsByTitle() {
-  var table = document.querySelector('table')
-  var thead = table.querySelector('thead')
-  var tbody = table.querySelector('tbody')
+  var table = document.querySelector('table');
+  var thead = table.querySelector('thead');
+  var tbody = table.querySelector('tbody');
 
   // NodeList
   // https://developer.mozilla.org/ru/docs/Web/API/Nodelist
-  rows = tbody.querySelectorAll('tr')
-  var sortedRows = []
+  rows = tbody.querySelectorAll('tr');
+  var sortedRows = [];
 
   // select all table rows
   for (var i = 0; i < rows.length; i++) {
-    sortedRows.push(rows[i])
+    sortedRows.push(rows[i]);
   }
 
   if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
-    sortedRows.sort(compareRowsAsc)
-    this.querySelector('.octicon-arrow-up').classList.remove('hide')
-    this.querySelector('.octicon-arrow-down').classList.add('hide')
+    sortedRows.sort(compareRowsAsc);
+    this.querySelector('.octicon-arrow-up').classList.remove('hide');
+    this.querySelector('.octicon-arrow-down').classList.add('hide');
   } else {
-    sortedRows.sort(compareRowsDesc)
-    this.querySelector('.octicon-arrow-up').classList.add('hide')
-    this.querySelector('.octicon-arrow-down').classList.remove('hide')
+    sortedRows.sort(compareRowsDesc);
+    this.querySelector('.octicon-arrow-up').classList.add('hide');
+    this.querySelector('.octicon-arrow-down').classList.remove('hide');
   }
 
-  var sortedTbody = document.createElement('tbody')
+  var sortedTbody = document.createElement('tbody');
   for (var i = 0; i < sortedRows.length; i++) {
-    sortedTbody.appendChild(sortedRows[i])
+    sortedTbody.appendChild(sortedRows[i]);
   }
 
-  tbody.parentNode.replaceChild(sortedTbody, tbody)
+  tbody.parentNode.replaceChild(sortedTbody, tbody);
 }
 
 function compareRowsAsc(row1, row2) {
-  var testTitleTag1 = row1.querySelector('th') ? row1.querySelector('th') : row1.querySelector('td')
-  var testTitleTag2 = row2.querySelector('th') ? row2.querySelector('th') : row2.querySelector('td')
-  var testTitle1 = testTitleTag1.textContent
-  var testTitle2 = testTitleTag2.textContent
-  if (testTitle1 < testTitle2) { return -1 }
-  if (testTitle1 > testTitle2) { return 1 }
-  return 0
+  var testTitleTag1 = row1.querySelector('th') ? row1.querySelector('th') : row1.querySelector('td');
+  var testTitleTag2 = row2.querySelector('th') ? row2.querySelector('th') : row2.querySelector('td');
+  var testTitle1 = testTitleTag1.textContent;
+  var testTitle2 = testTitleTag2.textContent;
+  if (testTitle1 < testTitle2) { return -1; }
+  if (testTitle1 > testTitle2) { return 1; }
+  return 0;
 }
 
 function compareRowsDesc(row1, row2) {
-  var testTitleTag1 = row1.querySelector('th') ? row1.querySelector('th') : row1.querySelector('td')
-  var testTitleTag2 = row2.querySelector('th') ? row2.querySelector('th') : row2.querySelector('td')
-  var testTitle1 = testTitleTag1.textContent
-  var testTitle2 = testTitleTag2.textContent
-  if (testTitle1 < testTitle2) { return 1 }
-  if (testTitle1 > testTitle2) { return -1 }
-  return 0
+  var testTitleTag1 = row1.querySelector('th') ? row1.querySelector('th') : row1.querySelector('td');
+  var testTitleTag2 = row2.querySelector('th') ? row2.querySelector('th') : row2.querySelector('td');
+  var testTitle1 = testTitleTag1.textContent;
+  var testTitle2 = testTitleTag2.textContent;
+  if (testTitle1 < testTitle2) { return 1; }
+  if (testTitle1 > testTitle2) { return -1; }
+  return 0;
 }
