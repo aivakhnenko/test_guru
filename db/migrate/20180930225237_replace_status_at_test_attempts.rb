bca8952e2e5fd1e382782f3e4d741c3ed88cf8a8
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2018-10-27 19:20:21 +0300

    Lesson 17 - fix for db:migrate on heroku

--- db/migrate/20180930225237_replace_status_at_test_attempts.rb
+++ db/migrate/20180930225237_replace_status_at_test_attempts.rb
@@ -1,7 +1,7 @@
 class ReplaceStatusAtTestAttempts < ActiveRecord::Migration[5.2]
   def change
     change_table :test_attempts do |t|
-      t.remove :status, :integer
+      t.remove :status
       t.integer :correct_questions, default: 0
       t.references :current_question, foreign_key: true
     end
