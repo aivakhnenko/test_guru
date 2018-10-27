2ce6ee096e52bc63d66738d3ca261aeba2f9b46f
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2018-10-27 19:32:45 +0300

    Lesson 17 - fix for db:migrate on heroku 3

--- db/migrate/20181011085620_change_name_and_type_at_users.rb
+++ db/migrate/20181011085620_change_name_and_type_at_users.rb
@@ -1,8 +1,8 @@
 class ChangeNameAndTypeAtUsers < ActiveRecord::Migration[5.2]
   def change
     change_table :users do |t|
-      t.remove :name, :string
-      t.remove :user_type, :integer
+      t.remove :name
+      t.remove :user_type
       t.string :first_name
       t.string :last_name
       t.string :type, null: false, default: 'User'
